8829aed9c3aaddf5713ea22da1bedc260349a805
James Gasior <jkole822@gmail.com>
2020-07-04 09:10:46 -0400

    defined port

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const Timestamp = require('./models/timestamp');
 
 const app = express();
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 const fccIndexPath = path.join(__dirname, '../views/index.html');
 
